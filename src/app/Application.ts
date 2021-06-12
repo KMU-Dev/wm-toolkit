5bd7a838d5b9f6e4686fb1f17a0ddbbd223d5e39
趙子賢 <danny900714@gmail.com>
2021-06-13 00:34:18 +0800

    Add remove winlock feature

--- src/app/Application.ts
+++ src/app/Application.ts
@@ -6,12 +6,28 @@ export default class Application {
     start() {
         // register commands
         this.registerCommand("下載影片", DownloadVideoCommand, "v");
+
+        // remove winlock
+        this.removeWinlock();
     }
 
     private registerCommand(name: string, Handler: Type<CommandHandler>, accessKey: string) {
         const handler = new Handler();
-        console.log(handler);
         GM_registerMenuCommand(name, handler.handle, accessKey);
+    }
+
+    private removeWinlock() {
+        if (window.location.pathname === "/learn/exam/exam_start.php") {
+            console.log("Exam started.");
+
+            new MutationObserver((_, observer) => {
+                const winlockScript = document.querySelector('script[src*="winlock"]');
+                if (winlockScript) {
+                    winlockScript.remove();
+                    observer.disconnect();
+                }
+            }).observe(document, { childList: true, subtree: true});
+        }
     }
 }
 
